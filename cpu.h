efabc52a854fc8792dd2141d02f29b7b24a8784a
Deseteral <deseteral@gmail.com>
2016-03-04 00:32:40 +0100

    Initial commit

---
+++ cpu.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "types.h"
+
+typedef struct CPU
+{
+  u32 pc;
+} CPU;
+
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "cpu.h"
+
+int main()
+{
+  CPU cpu;
+  cpu.pc = 2;
+
+  printf("%d\n", cpu.pc);
+
+  return 0;
+}
