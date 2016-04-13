09d3721b57ed5112b8d8b6ec024795b6cc2dd7e2
Deseteral <deseteral@gmail.com>
2016-04-13 15:25:03 +0200

    Added opcodes header

---
+++ src/opcodes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define OP_NOP  0x00
+#define OP_HCF  0x01
+#define OP_MOV  0x02
+#define OP_ADD  0x03
+#define OP_SUB  0x04
+#define OP_MUL  0x05
+#define OP_DIV  0x06
+#define OP_MOD  0x07
+#define OP_SHL  0x08
+#define OP_SHR  0x09
+#define OP_AND  0x0A
+#define OP_OR   0x0B
+#define OP_XOR  0x0C
+#define OP_NOT  0x0D
+#define OP_PUSH 0x0E
+#define OP_POP  0x0F
