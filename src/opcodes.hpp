86eda286eb05cab4bdd30cf7b20e12125fccdfaf
Deseteral <deseteral@gmail.com>
2016-04-30 10:02:50 +0200

    Added values to the opcodes.hpp

--- src/opcodes.hpp
+++ src/opcodes.hpp
@@ -36,3 +36,46 @@
 // extensions
 #define _OP_SIZE  0xF0
 #define _OP_DEBUG 0xF1
+
+// values
+#define VAL_R0  0x00
+#define VAL_R1  0x01
+#define VAL_R2  0x02
+#define VAL_R3  0x03
+#define VAL_R4  0x04
+#define VAL_R5  0x05
+#define VAL_R6  0x06
+#define VAL_R7  0x07
+#define VAL_R8  0x08
+#define VAL_R9  0x09
+#define VAL_R10 0x0A
+#define VAL_R11 0x0B
+#define VAL_R12 0x0C
+#define VAL_R13 0x0D
+#define VAL_R14 0x0E
+#define VAL_R15 0x0F
+
+#define VAL_LC  0x10
+#define VAL_SP  0x11
+#define VAL_PC  0x12
+
+#define VAL_AT_ADDR_R0  0x80
+#define VAL_AT_ADDR_R1  0x81
+#define VAL_AT_ADDR_R2  0x82
+#define VAL_AT_ADDR_R3  0x83
+#define VAL_AT_ADDR_R4  0x84
+#define VAL_AT_ADDR_R5  0x85
+#define VAL_AT_ADDR_R6  0x86
+#define VAL_AT_ADDR_R7  0x87
+#define VAL_AT_ADDR_R8  0x88
+#define VAL_AT_ADDR_R9  0x89
+#define VAL_AT_ADDR_R10 0x8A
+#define VAL_AT_ADDR_R11 0x8B
+#define VAL_AT_ADDR_R12 0x8C
+#define VAL_AT_ADDR_R13 0x8D
+#define VAL_AT_ADDR_R14 0x8E
+#define VAL_AT_ADDR_R15 0x8F
+
+#define VAL_AT_ADDR_LC  0x90
+#define VAL_AT_ADDR_SP  0x91
+#define VAL_AT_ADDR_PC  0x92
